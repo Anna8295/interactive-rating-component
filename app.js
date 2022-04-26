4e1d85b3f88a9e7d190ffe1ffaf7a5cc5ebf1eab
Annamaria <77789664+Anna8295@users.noreply.github.com>
2022-04-26 09:48:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,32 +2,16 @@ const btn = document.getElementById('submit');
 const thankYou = document.querySelector('#put-rate');
 const rate = document.getElementsByName('rate');
 
-const rateCard = document.getElementById('card-rate-active');
-const thankCard = document.getElementById('card-thankyou-active');
-
 const currentCard = localStorage.getItem('card')
 if (currentCard) {
 } if (currentCard === 'card-rate-active') {
     document.documentElement.setAttribute('current-card', currentCard);
 }
 
-//btn.addEventListener('click', function () {
-    //rate.forEach(radio => {
-        //if (radio.checked) {
-            //const finalRate = radio.value;}
-        //if (currentCard === rateCard) {
-            //document.documentElement.setAttribute('current-card', 'card-thankyou-active');
-            //localStorage.setItem('card', 'card-thankyou-active');
-           // thankYou.textContent = finalRate;
-       // }
-   // });
-//})
-
 btn.addEventListener('click', function () {
     rate.forEach(radio => {
         if (radio.checked) {
             const finalRate = radio.value;
-
             document.documentElement.setAttribute('current-card', 'card-thankyou-active');
             localStorage.setItem('card', 'card-thankyou-active');
             thankYou.textContent = finalRate;
@@ -36,12 +20,5 @@ btn.addEventListener('click', function () {
 })
 
 
-
-//if (currentCard){
-    //} if(currentCard === 'card-rate-active'){
-    //document.documentElement.setAttribute('current-card', currentCard);
-    //document.documentElement.setAttribute('current-card', 'card-thankyou-active');
-    //localStorage.setItem('card', 'card-thankyou-active');
-    //thankYou.textContent = finalRate; }
 
 
